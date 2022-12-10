812835c1819faf032c932da2ad86908efdec4863
Yogi Rahman Alif <yogismksab@gmail.com>
2022-12-10 18:06:39 +0000

    Build dan push docker image

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,11 @@
+#Build Docker image dari Dockerfile dengan nama <username-docker/karsajobs:latest
+echo "[+] Building docker image"
+docker build . -t ghcr.io/yogi-ra/karsajobs:latest
+
+#Login ke GitHub Packages
+echo "[+] Login GitHub Packages"
+echo $PASSWORD_GITHUB_PACKAGES | docker login ghcr.io -u yogi-ra --password-stdin
+
+#Push image ke Github Packages
+echo "[+] Push GitHub Packages"
+docker push ghcr.io/yogi-ra/karsajobs:latest
